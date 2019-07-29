0c0f4e25aa0db0ccff8062520c5375ad28544502
github cjl248 <github cjl248@cornell.edu>
2019-07-29 14:34:18 -0400

    four errors left

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+def build_network(call_letters, channel)
+  binding.pry
+  network_hash={}
+  network_hash[:call_letters] = call_letters
+  network_hash[:channel] = channel
+  Network.create(network_hash)
+end
+
 end
